fix error in datavail_all calculation, add figures with hydro & Xc overlaps

run_inputprep.py
@@ -173,8 +173,8 @@ plt.ylabel('variable')
 plt.legend()
 ax.grid(which='major', axis='both')
 imagedir = './figs/data/allavailabledata/'
-imagename = imagedir + 'data_availability_times.svg'
-fig.savefig(imagename, format='svg', dpi=1200)
+imagename = imagedir + 'data_availability_times.png'
+fig.savefig(imagename, format='png', dpi=1200)
 
 # Interp all data to full time series with identical sampling times
 dt = 3600
@@ -258,16 +258,65 @@ plt.plot(tplot,datavail_lidarwg090*6,'o',label='lidarwg x = 90m, N = '+str(sum(d
 plt.plot(tplot,datavail_lidarwg100*7,'o',label='lidarwg x = 100m, N = '+str(sum(datavail_lidarwg100)))
 plt.plot(tplot,datavail_lidarwg110*8,'o',label='lidarwg x = 110m, N = '+str(sum(datavail_lidarwg110)))
 plt.plot(tplot,datavail_lidarwg140*9,'o',label='lidarwg x = 140m, N = '+str(sum(datavail_lidarwg140)))
-tmp = datavail_wave8m + datavail_wave17m + datavail_tidegauge + datavail_lidar_elev2p + datavail_lidarwg090 + datavail_lidarwg100 + datavail_lidarwg110 + datavail_lidarwg140
+tmp = datavail_wave8m + datavail_wave17m + datavail_tidegauge + datavail_lidar_elev2p  + datavail_lidarwg100 + datavail_lidarwg110
 datavail_all = np.ones(shape=time_fullspan.shape)
-datavail_all[tmp > 1] = 0
-plt.plot(tplot,datavail_all*10,'o',label='all, N = '+str(sum(datavail_all)))
+datavail_all[tmp < 6] = 0
+plt.plot(tplot,datavail_all*10,'o',label='all but WG-80,90,140, N = '+str(sum(datavail_all)))
 plt.legend(bbox_to_anchor=(1.05, 1.0), loc='upper left')
 plt.tight_layout()
 plt.show()
 imagedir = './figs/data/allavailabledata/'
 imagename = imagedir + 'data_availability_timeoverlap.svg'
 fig.savefig(imagename, format='svg', dpi=1200)
+
+# Define when we have contour positions relative to hydro data
+datavail_Xc = np.ones(shape=cont_ts_fullspan.shape)
+datavail_dXcdt = np.ones(shape=dXContdt_fullspan.shape)
+fig, (ax1,ax2) = plt.subplots(2)
+for ii in np.arange(cont_elev.size):
+    tmp = np.isnan(cont_ts_fullspan[ii,:])
+    datavail_Xc[ii, tmp] = 0
+    tmp = np.isnan(dXContdt_fullspan[ii, :])
+    datavail_dXcdt[ii, tmp] = 0
+    tplot = pd.to_datetime(time_fullspan, unit='s', origin='unix')
+    ax1.plot(tplot,(ii+1)*datavail_Xc[ii, :],'o',label='Zc = '+str(cont_elev[ii]))
+    ax2.plot(tplot,(ii+1)*datavail_dXcdt[ii, :],'o',label='Zc = '+str(cont_elev[ii]))
+ax1.grid(which='both', axis='both')
+ax1.plot(tplot,datavail_all*10,'ok')
+ax2.grid(which='both', axis='both')
+ax2.plot(tplot,datavail_all*10,'ok')
+ax1.legend()
+# tmp = np.append(datavail_Xc.T,np.reshape(datavail_all,(time_fullspan.size,1)),axis=1)
+
+fig, (ax1,ax2) = plt.subplots(2)
+for ii in np.arange(cont_elev.size):
+    tmp = datavail_Xc[ii, :] + datavail_all
+    yplot = np.ones(shape=tplot.shape)
+    yplot[tmp < 2] = 0
+    ax1.plot(tplot,(ii+1)*yplot,'o',label='Zc = '+str(cont_elev[ii])+', N = '+str(sum(yplot)))
+    tmp = datavail_dXcdt[ii, :] + datavail_all
+    yplot = np.ones(shape=tplot.shape)
+    yplot[tmp < 2] = 0
+    ax2.plot(tplot, (ii + 1) * yplot, 'o', label='Zc = ' + str(cont_elev[ii]) + ', N = ' + str(sum(yplot)))
+ax1.grid(which='both', axis='both')
+# ax1.plot(tplot,datavail_all*10,'ok')
+ax2.grid(which='both', axis='both')
+# ax2.plot(tplot,datavail_all*10,'ok')
+ax1.legend(bbox_to_anchor=(1.05, 1.0), loc='upper left')
+ax2.legend(bbox_to_anchor=(1.05, 1.0), loc='lower left',draggable=True)
+ax1.set_title('Xc overlap with hydro data')
+ax1.set_xticks([])
+ax2.set_title('dXc/dt overlap with hydro data')
+plt.tight_layout()
+imagedir = './figs/data/allavailabledata/'
+imagename = imagedir + 'data&Xc_availability_timeoverlap.png'
+fig.savefig(imagename, format='png', dpi=1200)
+
+
+
+
+
+
 
 
 # Normalize output, simple method (remove mean, divide by standard deviatiion)