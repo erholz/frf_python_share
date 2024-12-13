fab58602cd6c08095fc6089c682335bb7730d8c3
erholz <elizabeth.holzenthal@gmail.com>
2024-12-13 08:25:29 -0600

    fix bug in create_contours.py

--- funcs/create_contours.py
+++ funcs/create_contours.py
@@ -19,15 +19,20 @@ def create_contours(elev_input,lidartime,lidar_xFRF,cont_elev):
                 if (np.nanmin(ztmp) <= cont_elev[cc]) & (np.nanmax(ztmp) >= cont_elev[cc]):
                     vartmp = abs(ztmp - cont_elev[cc]) == np.nanmin(abs(ztmp - cont_elev[cc]))
                     difftmp = (ztmp[0:-2]-cont_elev[cc])*(ztmp[1:-1]-cont_elev[cc])
-                    iiclosest = np.min(np.where(difftmp < 0))
-                    # iiclose = np.min(np.argwhere(vartmp)[0])
-                    # iicloserng = np.arange(iiclose-3,iiclose+3)
-                    # difftmp = (ztmp[iiclose-3:iiclose+3]-cont_elev[cc])*(ztmp[iiclose-2:iiclose+4]-cont_elev[cc])
-                    # if (sum(np.isnan(difftmp)) > 0) or (sum(difftmp < 0) == 0):
-                    #     iiclosest = iiclose
-                    # else:
-                    #     iiclosest = iicloserng[np.min(np.where(difftmp < 0))]
-                    xcc[cc] = np.interp(0, np.flip(ztmp[iiclosest:iiclosest + 2] - cont_elev[cc]), np.flip(xtmp[iiclosest:iiclosest + 2]))
+                    iiclosest = np.where(difftmp < 0)[0]
+                    if len(iiclosest) > 0:
+                        if iiclosest.size > 1:
+                            iiclosest = np.min(iiclosest)
+                        if isinstance(iiclosest, np.ndarray):
+                            iiclosest = iiclosest[0]
+                        # iiclose = np.min(np.argwhere(vartmp)[0])
+                        # iicloserng = np.arange(iiclose-3,iiclose+3)
+                        # difftmp = (ztmp[iiclose-3:iiclose+3]-cont_elev[cc])*(ztmp[iiclose-2:iiclose+4]-cont_elev[cc])
+                        # if (sum(np.isnan(difftmp)) > 0) or (sum(difftmp < 0) == 0):
+                        #     iiclosest = iiclose
+                        # else:
+                        #     iiclosest = iicloserng[np.min(np.where(difftmp < 0))]
+                        xcc[cc] = np.interp(0, np.flip(ztmp[iiclosest:iiclosest + 2] - cont_elev[cc]), np.flip(xtmp[iiclosest:iiclosest + 2]))
         cont_ts[:, tt] = xcc
     # Calculate mean and stddev of contour x-loc
     cmean = np.nanmean(cont_ts,axis=1)
